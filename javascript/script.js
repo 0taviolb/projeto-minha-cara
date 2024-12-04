2f4e5c4c041998a06577a8c16c83a6e06b450862
Otavio <eduardosecundario.user@gmail.com>
2024-12-04 14:08:20 -0300

    CommitFInal

---
+++ javascript/script.js
@@ -0,0 +1,67 @@
+document.querySelector("#btn1").onclick = () => document.querySelector("#dialog1").showModal();
+document.querySelector("#dialog1 button").onclick = () => document.querySelector("#dialog1").close();
+
+document.querySelector("#btn2").onclick = () => document.querySelector("#dialog2").showModal();
+document.querySelector("#dialog2 button").onclick = () => document.querySelector("#dialog2").close();
+
+
+document.querySelector("#btn3").onclick = () => document.querySelector("#dialog3").showModal();
+document.querySelector("#dialog3 button").onclick = () => document.querySelector("#dialog3").close();
+
+document.querySelector("#btn4").onclick = () => document.querySelector("#dialog4").showModal();
+document.querySelector("#dialog4 button").onclick = () => document.querySelector("#dialog4").close();
+
+document.querySelector("#btn5").onclick = () => document.querySelector("#dialog5").showModal();
+document.querySelector("#dialog5 button").onclick = () => document.querySelector("#dialog5").close();
+
+document.querySelector("#btn6").onclick = () => document.querySelector("#dialog6").showModal();
+document.querySelector("#dialog6 button").onclick = () => document.querySelector("#dialog6").close();
+
+document.querySelector("#btn7").onclick = () => document.querySelector("#dialog7").showModal();
+document.querySelector("#dialog7 button").onclick = () => document.querySelector("#dialog7").close();
+
+document.querySelector("#btn8").onclick = () => document.querySelector("#dialog8").showModal();
+document.querySelector("#dialog8 button").onclick = () => document.querySelector("#dialog8").close();
+
+document.querySelector("#btn9").onclick = () => document.querySelector("#dialog9").showModal();
+document.querySelector("#dialog9 button").onclick = () => document.querySelector("#dialog9").close();
+
+document.querySelector("#btn10").onclick = () => document.querySelector("#dialog10").showModal();
+document.querySelector("#dialog10 button").onclick = () => document.querySelector("#dialog10").close();
+
+document.querySelector("#btn11").onclick = () => document.querySelector("#dialog11").showModal();
+document.querySelector("#dialog11 button").onclick = () => document.querySelector("#dialog11").close();
+
+document.querySelector("#btn12").onclick = () => document.querySelector("#dialog12").showModal();
+document.querySelector("#dialog12 button").onclick = () => document.querySelector("#dialog12").close();
+
+document.querySelector("#btn13").onclick = () => document.querySelector("#dialog13").showModal();
+document.querySelector("#dialog13 button").onclick = () => document.querySelector("#dialog13").close();
+
+document.querySelector("#btn14").onclick = () => document.querySelector("#dialog14").showModal();
+document.querySelector("#dialog14 button").onclick = () => document.querySelector("#dialog14").close();
+
+document.querySelector("#btn15").onclick = () => document.querySelector("#dialog15").showModal();
+document.querySelector("#dialog15 button").onclick = () => document.querySelector("#dialog15").close();
+
+document.querySelector("#btn16").onclick = () => document.querySelector("#dialog16").showModal();
+document.querySelector("#dialog16 button").onclick = () => document.querySelector("#dialog16").close();
+
+
+document.querySelector("#btn17").onclick = () => document.querySelector("#dialog17").showModal();
+document.querySelector("#dialog17 button").onclick = () => document.querySelector("#dialog17").close();
+
+
+document.querySelector("#btn18").onclick = () => document.querySelector("#dialog18").showModal();
+document.querySelector("#dialog18 button").onclick = () => document.querySelector("#dialog18").close();
+
+document.querySelector("#btn19").onclick = () => document.querySelector("#dialog19").showModal();
+document.querySelector("#dialog19 button").onclick = () => document.querySelector("#dialog19").close();
+
+
+document.querySelector("#btn20").onclick = () => document.querySelector("#dialog20").showModal();
+document.querySelector("#dialog20 button").onclick = () => document.querySelector("#dialog20").close();
+
+
+document.querySelector("#btn21").onclick = () => document.querySelector("#dialog21").showModal();
+document.querySelector("#dialog21 button").onclick = () => document.querySelector("#dialog21").close();
